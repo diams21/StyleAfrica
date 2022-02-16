331e6471f7d7fc2f64a161ff6a24f8cc8c101329
tresor <trsordiambu2@gmail.com>
2022-02-16 17:08:07 +0100

    modif jusquala creation du filtre

---
+++ src/classec/Search.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Classe;
+use App\Entity\Category;
+
+class Search 
+{
+
+    /**
+    *@var String
+    */
+
+    public $string='';
+
+    /**
+    *@var Category[]
+    */
+
+    public $categories=[];
+
+
+
+}
